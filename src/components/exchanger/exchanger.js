fa4ba103e6233c77d8ae2ac8c38c549ea80eb4bc
JohnTamerZaki <johntamer98@gmail.com>
2022-07-28 21:45:18 +0200

    setting up the rates and the default rates

--- src/components/exchanger/exchanger.js
+++ src/components/exchanger/exchanger.js
@@ -1,5 +1,28 @@
 import "./exchanger.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+const BASE_URL_from =
+  "https://v6.exchangerate-api.com/v6/b3a5dde5ff3fa01bf5e615a8/latest/EUR";
+const BASE_URL_To =
+  "https://v6.exchangerate-api.com/v6/b3a5dde5ff3fa01bf5e615a8/latest/USD";
 export const Exchanger = () => {
+  const [fromCurrencyOption, setFromCurrencyOption] = useState([]);
+  const [toCurrencyOption, setToCurrencyOption] = useState([]);
+  console.log(fromCurrencyOption);
+  useEffect(() => {
+    axios
+      .get(BASE_URL_from)
+      .then((response) => {
+        console.log(response.data);
+        setFromCurrencyOption([...Object.keys(response.data.conversion_rates)]);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    axios.get(BASE_URL_To).then((response) => {
+      setToCurrencyOption([...Object.keys(response.data.conversion_rates)]);
+    });
+  }, []);
   return (
     <div className="exchanger">
       <div className="exchanger__left">
@@ -13,13 +36,21 @@ export const Exchanger = () => {
         <div>
           <div className="exchanger__from__text">From</div>
           <select className="exchanger__from">
-            <option value="hello">hello</option>
+            {fromCurrencyOption.map((option) => (
+              <option key={option} value={option}>
+                {option}
+              </option>
+            ))}
           </select>
         </div>
         <div>
           <div className="exchanger__to__text">To</div>
           <select className="exchanger__to">
-            <option value="hello">hello</option>
+            {toCurrencyOption.map((option) => (
+              <option key={option} value={option}>
+                {option}
+              </option>
+            ))}
           </select>
           <div>
             <button className="exchanger__btn">convert</button>
